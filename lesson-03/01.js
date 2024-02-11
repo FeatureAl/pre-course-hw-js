0434f6312bec8c1132285400babcff9b8573e96f
FeatureAl <alenfront@gmail.com>
2024-02-11 12:52:24 +0700

    03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,8 +1,7 @@
 ​const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    const flagColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
-    return flagColors.join("-");
+    return colors.filter(color => color === "черный" || color === "красный" || color === "желтый")('-');
 }
 
 console.log(createColorString());
