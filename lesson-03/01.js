b96bcae4d1b64dd01537848eb187376dafe89c4c
FeatureAl <alenfront@gmail.com>
2024-02-11 13:06:21 +0700

    03-01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function creatColorString() {
-    let germaFlag = colors.filter ( function(color) { return color === "черный"  color === "красный"  color === "желтый";    });
+    let germaFlag = colors.filter ( function(color) { return color === "черный" || color === "красный" || color === "желтый";    });
     return germaFlag.join('-');
 }
 
