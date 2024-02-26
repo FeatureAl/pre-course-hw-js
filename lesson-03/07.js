d5557bd11b22e28695a9ca98fd11e75eb37385bf
FeatureAl <alenfront@gmail.com>
2024-02-26 13:31:17 +0700

    03.07

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -2,8 +2,8 @@ const numbers = [
     [1, 2, 3, 4, 5],
     [11, 20, 33, 40, 55],
     [111, 200, 333, 400, 555],
-  ];
+];
 
-  let newArr = numbers.map(row => row.filter(num => num % 2 !== 0));
+let newArr = numbers.map(row => row.filter(num => num % 2 === 0));
 
-  console.log(JSON.stringify(newArr));
+console.log(newArr);
